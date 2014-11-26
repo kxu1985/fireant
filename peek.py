52e244116cb79a9034729a2e0cb1bcf7dea38bef
Ke Xu <kxu1985@gmail.com>
2014-11-26 11:39:41 -0800

    update peek.py

--- peek.py
+++ peek.py
@@ -2,6 +2,10 @@
 import sys
 import subprocess
 import time
+import pickle
+from xml.etree import ElementTree
+import base64
+
 
 blockID = 1
 
@@ -9,14 +13,35 @@ blockID = 1
 # Called by Plugin
 # Input file: rsreq.json
 #=========================
-reqfile = 'rsreq.json'
-subprocess.call("ccnputfile -v ccnx:/rsrepo/rsreq.json ~/Documents/fireant/rsreq.json",shell=True)
+reqfilename = 'req_' + str(blockID) + '_' + str(time.time()) + '.json'
+subprocess.call("ccnputfile -v ccnx:/rsrepo/" + reqfilename + \
+                " ~/Documents/fireant/rsreq.json",shell=True)
 
 #===========================
 # send out resource request
 #===========================
-lifetime = 3
+lifetime = 10
 waittime = lifetime
-reqID = 'ccnx:/fireant/req_' + str(blockID) + '_' + str(time.time())
-proc = subprocess.Popen(['ccnpeek','-v','-l',str(lifetime),'-w',str(waittime),reqID], stdout=subprocess.PIPE).communicate()
-print proc
+reqURL = 'ccnx:/fireant/' + reqfilename
+proc = subprocess.Popen(['ccnpeek','-v','-l',str(lifetime),'-w',str(waittime),reqURL], \
+                        stdout=subprocess.PIPE).communicate()
+if proc[0] != '':
+  #-------------------------------
+  # Analyze the returned response
+  #-------------------------------
+  f = open('/tmp/recv_bin', 'w')
+  f.write(proc[0])
+  f.close()
+  subprocess.call("ccn_ccnbtoxml -v /tmp/recv_bin | xmllint --format - \
+                  > /tmp/recv_xml",shell=True)
+  xmlorigin = ElementTree.parse('/tmp/recv_xml')
+  #print xmlorigin
+  xmlcontent = xmlorigin.find('Content')
+  #print xmlcontent.tag
+  #print xmlcontent.attrib
+  print xmlcontent.text
+  #print xmlcontent.tail
+  responsefile = base64.b64decode(xmlcontent.text).strip()
+  print responsefile
+  subprocess.call("ccngetfile -v ccnx:/rsrepo/" + responsefile + " /tmp/" + responsefile, \
+                  shell=True) 
